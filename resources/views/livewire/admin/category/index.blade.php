9ea9bcd1dcece968019024774f258c1f00186fae
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-01-09 21:18:54 +0330

    remove some icons

--- resources/views/livewire/admin/category/index.blade.php
+++ resources/views/livewire/admin/category/index.blade.php
@@ -45,10 +45,10 @@
                             <div class="field-placeholder">جستجو <span class="text-danger">*</span></div>
                         </div>
                         <div class="d-flex">
-                             <a  href="{{route('admin.category.create')}}" class="btn btn-success mb-3"
-                                style="margin-left: 20px">افزودن دسته بندی جدید <span
-                                     class="me-1 icon-plus-circle"></span>
-                             </a>
+                            <a href="{{route('admin.category.create')}}" class="btn btn-success mb-3"
+                               style="margin-left: 20px">افزودن دسته بندی جدید <span
+                                    class="me-1 icon-plus-circle"></span>
+                            </a>
                             {{ $allCategory->links('layouts.pagination-admin') }}
                         </div>
                     </div>
@@ -78,121 +78,15 @@
                                                      class=" ms-2 media-avatar"
                                                      alt="Product">
                                             </div>
-                                            <form class="mt-2 d-inline-flex align-items-center p-1"
-                                                  style="background: #575757;border-radius: 5px"
-                                                  wire:submit.prevent="categoryThumbnail({{$category->id}},'{{@$category->image->file}}')">
-                                                <input style="display: -webkit-inline-box;" type="file"
-                                                       wire:model="categoryThumbnail">
-                                                <button class="btn btn-sm btn-success" type="submit">
-                                                    ذخیره
-                                                    <div wire:loading
-                                                         wire:target="categoryThumbnail({{$category->id}},'{{@$category->image->file}}')"
-                                                         class="spinner-border spinner-border-sm"></div>
-                                                </button>
-
-                                            </form>
-                                        </td>
-                                        <td>
-
-                                            <div class="field-wrapper">
-                                                <input name="title" class="form-control" type="text"
-                                                       value="{{@$category->title }}"
-                                                       wire:change="updateCategoryTitle($event.target.value,{{ $category->id }})">
-                                                <div wire:loading class="spinner-border spinner-border-sm" role="status"
-                                                     style="position: absolute;left: 13px;top: 12px;color: #a0a0a0;z-index: 100000">
-                                                    <span class="visually-hidden">Loading...</span>
-                                                </div>
-                                            </div>
-
-                                        </td>
-                                        <td>
-                                            <div class="field-wrapper">
-                                                <select wire:ignore disabled
-                                                        class="js-states"
-                                                        name="category_id" id="category_id"
-                                                        title="Select Product Category"
-                                                        data-live-search="true"
-                                                        class="form-select @error('category_id') error-input-border @enderror">
-
-                                                    <option value="0">دسته والد</option>
-                                                    @foreach($categories as $item)
-                                                        <option
-                                                            @if($item->id==$category->category_id)
-                                                                selected
-                                                            @endif
-                                                            value="{{ $item->id }}">{{ $item->title }}</option>
-                                                    @endforeach
-                                                </select>
-                                                @foreach($errors->get('category_id') as $message)
-                                                    <div wire:loading.remove
-                                                         class="text-white d-flex invalid-tooltip">{{$message}}</div>
-                                                @endforeach
-                                            </div>
-
-                                            @if($category->category_id ==0)
-                                                <span>parent</span>
-                                            @elseif(isset($category->category_id))
-
-                                                {{@$category->category->title }}
-                                            @endif
-                                        </td>
-                                        <td>
-                                            @if($category->category_id==0)
-                                                <div class="field-wrapper">
-                                                    <select
-                                                        class="js-states"
-                                                        name="user_level_" id="category_id"
-                                                        data-live-search="true"
-                                                        class="form-select @error('category_id') error-input-border @enderror">
-
-                                                        @foreach($userLevels as $level)
-                                                            <option
-                                                                @if($level->id==$category->user_level_id)
-                                                                    selected
-                                                                @endif
-                                                                value="{{ $level->id }}">{{ $level->title }}</option>
-                                                        @endforeach
-                                                    </select>
-                                                    @foreach($errors->get('category_id') as $message)
-                                                        <div wire:loading.remove
-                                                             class="text-white d-flex invalid-tooltip">{{$message}}</div>
-                                                    @endforeach
-                                                </div>
-                                            @endif
-
-
-                                        </td>
                                         <td>
                                             <div class="actions t1">
-
-                                                <a href="{{route('client.category',$category->slug)}}" target="_blank"
-                                                   data-bs-toggle="tooltip" data-bs-placement="bottom"
-                                                   data-bs-original-title="صفحه دسته بندی">
-                                                    <i class="icon-open_in_new text-white ms-2"></i>
-                                                </a>
-                                                <a href="{{route('admin.category.feature',$category->id)}}"
-                                                   data-bs-toggle="tooltip" data-bs-placement="bottom"
-                                                   data-bs-original-title="ویژگی">
-                                                    <i class="icon-add_box text-white ms-2"></i>
-                                                </a>
-                                                <a href="{{route('admin.category.rule',$category->id)}}"
-                                                   data-bs-toggle="tooltip" data-bs-placement="bottom"
-                                                   data-bs-original-title="قوانین">
-                                                    <i class="icon-attach_file text-danger ms-2"></i>
-                                                </a>
-                                                <a href="{{route('admin.category.faq',$category->id)}}"
-                                                   data-bs-toggle="tooltip" data-bs-placement="bottom"
-                                                   data-bs-original-title="Faq">
-                                                    <i class="icon-file-text text-danger ms-2"></i>
-                                                </a>
                                                 <a href="javascript:0"
                                                    wire:click="editCategory({{$category->id}},{{$category->category_id}})"
                                                    data-bs-toggle="tooltip" data-bs-placement="bottom"
                                                    data-bs-original-title="ویرایش">
                                                     <i class="icon-edit1 text-info ms-2"></i>
                                                 </a>
-                                                <a href="javascript:0"
-                                                   {{$adminType->id!=1?'hidden':''}} wire:click="deleteConfirm({{$category->id}})"
+                                                <a href="javascript:0" wire:click="deleteConfirm({{$category->id}})"
                                                    data-bs-toggle="tooltip" data-bs-placement="bottom"
                                                    data-bs-original-title="حذف">
                                                     <i class="icon-x-circle text-danger ms-2"></i>
