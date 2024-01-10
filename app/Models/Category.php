1dac0269c8e61981a624d65a41ab3166ce3a5c23
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-01-10 06:42:25 +0330

    change some code

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -55,14 +55,14 @@ class Category extends Model
                     $extension = $image->extension();
                     $image_name = 'image_categories_' . $service_id . '_' . '_categories_' . Str::random(10) . time() . '.' . $extension;
                     $path = 'images/categories/' . $service_id . '/' . $image_name;
-                   Image::make($image)->save(public_path('images/categories/' . $service_id . '/' . $image_name), 40);
+                    Image::make($image)->save(public_path('images/categories/' . $service_id . '/' . $image_name), 40);
                     $this->insertImageToFileTable1($path, $cat_id, $service_id);
                 }
             }
         });
     }
 
-    public function insertImageToFileTable1($path, $service_id)
+    public function insertImageToFileTable1($path, $cat_id, $service_id)
     {
 
         return \App\Models\File::query()->updateOrCreate(
@@ -76,33 +76,8 @@ class Category extends Model
         );
     }
 
-    public function categoryThumbnail($categoryId, $oldPhoto, $categoryThumbnail)
-    {
-        $path = public_path() . '/images/categories/' . $categoryId;
-        if (!\Illuminate\Support\Facades\File::exists($path)) {
-            \Illuminate\Support\Facades\File::makeDirectory($path);
-        }
-        $extension = $categoryThumbnail->extension();
-        $image_name = 'image_categories_' . $categoryId . '_' . '_categories_' . Str::random(10) . time() . '.' . $extension;
-        $path = 'images/categories/' . $categoryId . '/' . $image_name;
-       Image::make($categoryThumbnail)->save(public_path($path),'50');
-        $this->removeOldImage($oldPhoto, $categoryId);
-        $this->insertImageToFileTable1($path, $categoryId);
-    }
-
-    public function removeOldImage($oldPhoto, $categoryId)
-    {
-        \Illuminate\Support\Facades\File::delete($oldPhoto);
-        /*   return \App\Models\File::query()->where([
-               'service_id' => $categoryId,
-               'type' => 'category',
-               'file' => $oldPhoto,
-           ])->delete();*/
-    }
-
     public function category()
     {
-
         return $this->belongsTo(Category::class, 'category_id');
     }
 
@@ -111,27 +86,8 @@ class Category extends Model
         return $this->hasMany(Category::class, 'category_id');
     }
 
-
-    public function faqs()
-    {
-        return $this->hasMany(Faq::class, 'category_id')->where('type', '=', 'category');
-
-    }
-
-    public function services()
-    {
-        return $this->hasMany(Service::class, 'category_id');
-
-    }
-
     public function image()
     {
         return $this->belongsTo(\App\Models\File::class, 'id', 'service_id')->where('type', '=', 'category');
-    }
-
-    public function categoryFeatures()
-    {
-        return $this->hasMany(CategoryFeature::class, 'category_id');
-
     }
 }
