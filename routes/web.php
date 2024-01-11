4e97e1902526ed1bb8bb2fbfe978a81cab1ab634
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-01-11 19:50:10 +0330

    added CategoryFeature route

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::group(['prefix' => 'admin'
         Route::get('/dashboard', Index::class)->name('dashboard');
         Route::get('/category', \App\Http\Livewire\Admin\Category\Index::class)->name('category');
         Route::get('/category/create', \App\Http\Livewire\Admin\Category\Create::class)->name('category.create');
+        Route::get('/category/feature/{category_id}', \App\Http\Livewire\Admin\Category\Feature::class)->name('category.feature');
         Route::get('/product', \App\Http\Livewire\Admin\Product\Index::class)->name('product');
         Route::get('/product/create', \App\Http\Livewire\Admin\Product\Create::class)->name('product.create');
 
