a7f38a8fe787c32f22b93018fe824d9f327ac3c4
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-01-11 08:34:10 +0330

    added layouts view admin to product index

--- app/Http/Livewire/Admin/Product/Index.php
+++ app/Http/Livewire/Admin/Product/Index.php
@@ -8,6 +8,6 @@ class Index extends Component
 {
     public function render()
     {
-        return view('livewire.admin.product.index');
+        return view('livewire.admin.product.index')->layout('layouts.app-admin');
     }
 }
