5413970995bf87d16d78a01aed3a3c4372e7c105
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-01-11 19:52:38 +0330

    added category feature model

--- app/Models/CategoryFeature.php
+++ app/Models/CategoryFeature.php
@@ -9,4 +9,20 @@ class CategoryFeature extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function saveFeature($formData, $category_id, $feature_id)
+    {
+        CategoryFeature::query()->updateOrCreate(
+            [
+                'id' => $feature_id
+            ],
+            [
+                'title' => $formData['title'],
+                'description' => $formData['description'],
+                'category_id' => $category_id,
+            ]
+        );
+    }
+
+
 }
