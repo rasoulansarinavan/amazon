1ad1b128f0421b090c8178a5264234efd2c01d1a
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-01-13 20:57:30 +0330

    added columns in features table

--- database/migrations/2024_01_13_154656_create_features_table.php
+++ database/migrations/2024_01_13_154656_create_features_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('features', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->foreignId('category_id');
             $table->timestamps();
         });
     }
