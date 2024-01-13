79c5d0c1f207df9b386ecc7de3db63b92c80622e
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-01-13 09:30:16 +0330

    add brand CRUD operation in brand model

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -4,10 +4,73 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class Brand extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+    protected $with = ['image'];
+
+    public function saveBrand($formData, $brand_id, $image)
+    {
+        DB::transaction(function () use ($formData, $brand_id, $image) {
+            $brand = Brand::query()->updateOrCreate(
+                [
+                    'id' => $brand_id
+                ],
+                [
+                    'persian_name' => $formData['persian_name'],
+                    'original_name' => $formData['original_name'],
+                    'slug' => Str::slug($formData['persian_name'], '-', null),
+                ]
+            );
+
+            $brandId = $brand->id;
+            $path = public_path() . '/images/brands/' . $brandId;
+            if (!\Illuminate\Support\Facades\File::exists($path)) {
+                \Illuminate\Support\Facades\File::makeDirectory($path);
+            }
+            if ($brandId == 0) {
+                $extension = $image->extension();
+                $image_name = 'image_brands_' . $brandId . '_' . '_brands_' . Str::random(10) . time() . '.' . $extension;
+                $path = 'images/brands/' . $brandId . '/' . $image_name;
+                Image::make($image)->save(public_path('images/brands/' . $brandId . '/' . $image_name), 40);
+                $this->insertImageToFileTable1($path, $brandId, $brand_id);
+            }
+
+            $adminFileName = File::query()->where([
+                'service_id' => $brand_id,
+                'type' => 'brand',
+            ])->pluck('file')->first();
+
+            if ($image != $adminFileName && $brand_id != 0) {
+                if (isset($image)) {
+                    unlink($adminFileName);
+                    $extension = $image->extension();
+                    $image_name = 'image_brands_' . $brandId . '_' . '_brands_' . Str::random(10) . time() . '.' . $extension;
+                    $path = 'images/brands/' . $brandId . '/' . $image_name;
+                    Image::make($image)->save(public_path('images/brands/' . $brandId . '/' . $image_name), 40);
+                    $this->insertImageToFileTable1($path, $brandId, $brand_id);
+                }
+            }
+        });
+    }
+
+    public function insertImageToFileTable1($path, $brandId, $brand_id)
+    {
+
+        return \App\Models\File::query()->updateOrCreate(
+            [
+                'service_id' => $brandId,
+                'type' => 'brand',
+            ],
+            [
+                'file' => $path,
+            ]
+        );
+    }
 }
