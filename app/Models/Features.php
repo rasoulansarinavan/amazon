fdd80ed9b12fd3250cd35ecee97e9d76c2529319
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-01-13 20:55:16 +0330

    add CRUD operation in features model

--- app/Models/Features.php
+++ app/Models/Features.php
@@ -8,4 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class Features extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+
+    public function saveFeature($formData, $feature_id, $category_id)
+    {
+   
+        Features::query()->updateOrCreate(
+            [
+                'id' => $feature_id,
+                'category_id' => $category_id,
+            ],
+            [
+                'title' => $formData['title'],
+            ]
+        );
+    }
+
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function featureValue()
+    {
+        return $this->belongsTo(Features::class, 'id', 'feature_id');
+    }
 }
