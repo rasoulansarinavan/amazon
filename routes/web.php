8fbf3ba32a58c73f40bb7b23c0bcce16762e426a
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-01-13 20:54:27 +0330

    refactor the feature route in web.php

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::group(['prefix' => 'admin'
         Route::get('/dashboard', Index::class)->name('dashboard');
         Route::get('/category', \App\Http\Livewire\Admin\Category\Index::class)->name('category');
         Route::get('/category/create', \App\Http\Livewire\Admin\Category\Create::class)->name('category.create');
-        Route::get('/category/feature/{category_id}', \App\Http\Livewire\Admin\Category\Feature::class)->name('category.feature');
+        Route::get('/category/feature/{id}', \App\Http\Livewire\Admin\Category\Feature::class)->name('category.feature');
         Route::get('/product', \App\Http\Livewire\Admin\Product\Index::class)->name('product');
         Route::get('/product/create', \App\Http\Livewire\Admin\Product\Create::class)->name('product.create');
         Route::get('/brand', \App\Http\Livewire\Admin\Brand\Index::class)->name('brand');
