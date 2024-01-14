e29b4edf8ee813f2530887fe8735136d30344ebb
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-01-14 09:38:09 +0330

    added CRUD operation in Product index component

--- app/Http/Livewire/Admin/Product/Index.php
+++ app/Http/Livewire/Admin/Product/Index.php
@@ -2,12 +2,53 @@
 
 namespace App\Http\Livewire\Admin\Product;
 
+use App\Models\File;
+use App\Models\Product;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public function special($value)
+    {
+        $product = Product::query()->where('id', $value)->first();
+
+        if ($product->special == 0) {
+            Product::query()->where('id', $value)->update(['special' => 1]);
+        } elseif ($product->special == 1) {
+            Product::query()->where('id', $value)->update(['special' => 0]);
+        }
+        $this->dispatchBrowserEvent('swal:alert-success');
+    }
+
+    public function show($value)
+    {
+        $product = Product::query()->where('id', $value)->first();
+
+        if ($product->active == 0) {
+            Product::query()->where('id', $value)->update(['active' => 1]);
+        } elseif ($product->active == 1) {
+            Product::query()->where('id', $value)->update(['active' => 0]);
+        }
+        $this->dispatchBrowserEvent('swal:alert-success');
+    }
+
+    public function deleteConfirm($id)
+    {
+        $this->dispatchBrowserEvent('swal:confirm', [
+            'id' => $id,
+        ]);
+    }
+
+    public function delete($product_id)
+    {
+        Product::query()->where('id', $product_id)->delete();
+        File::query()->where('service_id', $product_id)->delete();
+        $this->dispatchBrowserEvent('swal:alert-success');
+    }
+
     public function render()
     {
-        return view('livewire.admin.product.index')->layout('layouts.app-admin');
+        $products = Product::query()->get();
+        return view('livewire.admin.product.index', ['products' => $products])->layout('layouts.app-admin');
     }
 }
