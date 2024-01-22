8e153bdc82d46afc0f5381b09c28da58f614f6ac
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-01-22 11:54:30 +0330

    add relation in order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,7 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class order extends Model
+class Order extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'id', 'product_id');
+    }
+
+    public function payment()
+    {
+        return $this->belongsTo(Payment::class, 'user_id');
+    }
+
+    public function delivery()
+    {
+        return $this->belongsTo(delivery::class, 'id', 'delivery_id');
+    }
 }
