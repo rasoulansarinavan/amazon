a702d4346ce9b9dee12b6a08b61e184d4980896c
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-01-22 09:37:12 +0330

    add columns in order table

--- database/migrations/2024_01_22_053818_create_orders_table.php
+++ database/migrations/2024_01_22_053818_create_orders_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('payment_id')->nullable()->constrained('payments')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('delivery_id')->nullable()->constrained('delivery')->onUpdate('cascade')->onDelete('cascade');
+            $table->tinyInteger('order_status')->default(0);
             $table->timestamps();
         });
     }
