786949adf7e9cefffe6afa46cd305d0f16bff813
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-01-22 09:33:49 +0330

    add columns in payment table

--- database/migrations/2024_01_21_060016_create_payments_table.php
+++ database/migrations/2024_01_21_060016_create_payments_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->decimal('amount', 20, 3);
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->tinyInteger('status')->default(0);
+            $table->tinyInteger('type')->default(0)->comment('0 => online, 1 => offline, 2 => cash');
+            $table->unsignedBigInteger('paymentable_id');
+            $table->string('paymentable_type');
             $table->timestamps();
         });
     }
