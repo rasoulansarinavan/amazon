ca393b9195b56d273d54eb428abb7cc4ec8b431f
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-01-29 09:21:43 +0330

    refactor the view product

--- resources/views/livewire/admin/product/create.blade.php
+++ resources/views/livewire/admin/product/create.blade.php
@@ -3,33 +3,48 @@
         .select2-results__option, .select2-container--default .select2-selection--multiple .select2-selection__choice__display {
             text-transform: uppercase;
         }
+
     </style>
+    <!-- Content wrapper start -->
     <div class="content-wrapper">
+
+        <!-- Row start -->
         <div class="row">
             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+
+                <!-- Card start -->
                 <div class="card">
                     <div class="card-body">
                         <div class="custom-tabs-container">
+                            <a href="{{route('admin.product')}}" class="btn btn-outline-success"
+                               style="float: left">محصولات<span></span>
+                            </a>
                             <ul class="nav nav-tabs" id="myTab" role="tablist">
                                 <li class="nav-item" role="presentation">
-                                    <a disabled class="nav-link {{$step1 ? 'active' : '' }}" id="first-tab" data-bs-toggle="tab"
+                                    <a class="nav-link {{$step1 ? 'active' : '' }}" id="first-tab" data-bs-toggle="tab"
+                                       disabled
                                        role="tab" aria-controls="first" aria-selected="{{$step1 ? 'true' : '' }}">اطلاعات
                                         کلی</a>
                                 </li>
                                 <li class="nav-item" role="presentation">
-                                    <a disabled class="nav-link {{$step2 ? 'active' : '' }}" id="second-tab" data-bs-toggle="tab"
+                                    <a class="nav-link {{$step2 ? 'active' : '' }}" id="second-tab" data-bs-toggle="tab"
+                                       disabled
                                        role="tab"
                                        aria-controls="second" aria-selected="{{$step2 ? 'true' : '' }}">تصویر محصول</a>
                                 </li>
                                 <li class="nav-item" role="presentation">
                                     <a class="nav-link {{$step3 ? 'active' : '' }}" id="third-tab" data-bs-toggle="tab"
-                                      disabled role="tab"
+                                       disabled
+                                       role="tab"
                                        aria-controls="third" aria-selected="{{$step3 ? 'true' : '' }}">سئو</a>
                                 </li>
+
                                 <li class="nav-item" role="presentation">
-                                    <a class="nav-link {{$step4 ? 'active' : '' }}" id="fourth-tab" data-bs-toggle="tab"
-                                       disabled role="tab"
-                                       aria-controls="fourth" aria-selected="{{$step4 ? 'true' : '' }}">ویژگی محصول</a>
+                                    <a class="nav-link {{$step4 ? 'active' : '' }}" id="forth-tab" disabled
+                                       data-bs-toggle="tab"
+                                       role="tab"
+                                       aria-controls="forth" aria-selected="{{$step4 ? 'true' : '' }}">ویژگی
+                                        محصول</a>
                                 </li>
                             </ul>
                             <div class="tab-content" id="myTabContent">
@@ -48,7 +63,9 @@
         </div>
     </div>
     @push('script')
+        <!-- Summernote JS -->
         <script src="/backend/vendor/summernote/summernote-bs4.js"></script>
+        <!-- Input Tags JS -->
         <script src="/backend/vendor/input-tags/tagsinput.min.js"></script>
         <script src="/backend/vendor/input-tags/typeahead.js"></script>
         <script src="/backend/vendor/input-tags/tagsinput-custom.js"></script>
