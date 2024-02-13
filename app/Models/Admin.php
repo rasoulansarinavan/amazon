bbe6e4529fe22fe2118ba5cdb1276cc21b0692ac
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-13 09:41:50 +0330

    added admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,9 +3,42 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    use HasFactory;
+
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'mobile',
+        'password',
+        'block',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    public function image()
+    {
+        return $this->belongsTo(File::class, 'id', 'service_id')->where('type', '=', 'admin');
+    }
+
+
 }
