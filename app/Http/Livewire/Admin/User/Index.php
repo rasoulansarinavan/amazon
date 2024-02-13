4e2c26fa2907c0138ba4f2829456bea18738debd
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-13 09:44:43 +0330

    added index user component

--- app/Http/Livewire/Admin/User/Index.php
+++ app/Http/Livewire/Admin/User/Index.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Livewire\Admin\User;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.admin.user.index')->layout('layouts.app-admin');
+        $users = User::query()->get();
+        return view('livewire.admin.user.index',compact('users'))->layout('layouts.app-admin');
     }
 }
