ea8805c7ecd33192ab1d05d44b1ff2f151eb2b00
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-13 10:21:01 +0330

    added layouts in comment component

--- app/Http/Livewire/Admin/Comment/Index.php
+++ app/Http/Livewire/Admin/Comment/Index.php
@@ -8,6 +8,6 @@ class Index extends Component
 {
     public function render()
     {
-        return view('livewire.admin.comment.index');
+        return view('livewire.admin.comment.index')->layout('layouts.app-admin');
     }
 }
