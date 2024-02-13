409ed4d97bd2a85147211952e921a91782c9cfb0
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-13 09:43:31 +0330

    create DeleteAdmin

---
+++ app/Actions/Admin/DeleteAdmin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Actions\Admin;
+
+use App\Repositories\AdminRepository;
+
+class DeleteAdmin
+{
+    public function __construct(
+        private readonly AdminRepository $adminRepository
+    )
+    {
+    }
+
+    public function execute($value)
+    {
+        $this->adminRepository->deleteById($value);
+    }
+}
