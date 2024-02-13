7b355b951b3b572502cbac904522d23836315e51
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-13 09:43:41 +0330

    create EditAdmin

---
+++ app/Actions/Admin/EditAdmin.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Actions\Admin;
+
+use App\Repositories\AdminRepository;
+
+class EditAdmin
+{
+    public function __construct(
+        private readonly AdminRepository $adminRepository
+    )
+    {
+    }
+
+    public function execute($value)
+    {
+        $admin = $this->adminRepository->findById($value);
+        $image = $this->adminRepository->findFileById($value);
+
+        @$this->image = $image->file;
+        @$this->oldPhoto = $image->file;
+
+
+        $this->name = $admin->name;
+        $this->email = $admin->email;
+        $this->mobile = $admin->mobile;
+        $this->password = $admin->password;
+        $this->adminId = $admin->id;
+    }
+}
