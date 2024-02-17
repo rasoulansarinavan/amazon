6c77b194eb8db116e649f1757dca55458266b419
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-17 09:34:46 +0330

    create CommentRepository

---
+++ app/Repositories/CommentRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories;
+
+
+use App\Models\Comment;
+
+class CommentRepository
+{
+    public function findById(int $id)
+    {
+        return Comment::query()->where('id', $id)->first();
+    }
+
+    public function updateById(int $id, array $data)
+    {
+        Comment::query()->where('id', $id)->update($data);
+    }
+
+}
