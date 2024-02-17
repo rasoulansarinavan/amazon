58c53dd0b0ee2e18c0c65ba06ebd470de917377c
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-17 09:50:54 +0330

    refactor the show and added delete in comment component

--- app/Http/Livewire/Admin/Comment/Index.php
+++ app/Http/Livewire/Admin/Comment/Index.php
@@ -3,22 +3,37 @@
 namespace App\Http\Livewire\Admin\Comment;
 
 use App\Actions\Comment\ApprovedComment;
+use App\Actions\Comment\DeleteComment;
 use App\Actions\Comment\ShowComment;
 use App\Models\Comment;
 use Livewire\Component;
 
 class Index extends Component
 {
+    protected $listeners = ['delete'];
+
     public function approved($value, ApprovedComment $approvedComment)
     {
         $approvedComment->execute($value);
         $this->dispatchBrowserEvent('swal:alert-success');
     }
 
-    public function show($value,ShowComment $showComment)
+    public function show($value, ShowComment $showComment)
     {
         $showComment->execute($value);
         $this->dispatchBrowserEvent('swal:alert-success');
+    }
+
+    public function deleteConfirm($id)
+    {
+        $this->dispatchBrowserEvent('swal:confirm', [
+            'id' => $id,
+        ]);
+    }
+
+    public function delete($value, DeleteComment $deleteComment)
+    {
+        $deleteComment->execute($value);
     }
 
     public function render()
