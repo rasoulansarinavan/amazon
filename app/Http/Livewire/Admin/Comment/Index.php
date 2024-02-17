0b9bbd4d752915c95aa7b9cc1645bea5b6a71a97
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-17 09:35:43 +0330

    added approved pubf in comment component

--- app/Http/Livewire/Admin/Comment/Index.php
+++ app/Http/Livewire/Admin/Comment/Index.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Livewire\Admin\Comment;
 
+use App\Actions\Comment\ApprovedComment;
 use App\Models\Comment;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public function approved($value, ApprovedComment $approvedComment)
+    {
+        $approvedComment->execute($value);
+        $this->dispatchBrowserEvent('swal:alert-success');
+    }
+
     public function render()
     {
         $comments = Comment::query()->get();
