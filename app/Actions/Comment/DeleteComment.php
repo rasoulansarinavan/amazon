5e4c5805d53c3f7df19d18a041012cd21657437e
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-17 09:50:15 +0330

    create DeleteComment in comment actions

---
+++ app/Actions/Comment/DeleteComment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Actions\Comment;
+
+use App\Repositories\CommentRepository;
+
+class DeleteComment
+{
+    public function __construct(
+        private readonly CommentRepository $commentRepository
+    )
+    {
+    }
+
+    public function execute($value)
+    {
+        $this->commentRepository->DeleteById($value);
+    }
+}
