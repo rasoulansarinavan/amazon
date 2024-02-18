90a55fcc2f7513b13377c09e49bc01db38c71a65
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-18 20:00:40 +0330

    added answer comment component

--- app/Http/Livewire/Admin/Comment/Answer.php
+++ app/Http/Livewire/Admin/Comment/Answer.php
@@ -2,16 +2,37 @@
 
 namespace App\Http\Livewire\Admin\Comment;
 
+use App\Actions\Comment\AnswerComment;
 use App\Models\Comment;
+use Illuminate\Support\Facades\Validator;
 use Livewire\Component;
 
 class Answer extends Component
 {
-    public $comment;
+    public $comment, $body = '', $answerId;
 
     public function mount($id)
     {
         $this->comment = Comment::query()->where('id', $id)->firstOrFail();
+    }
+
+    public function answer($formData, Comment $comments, AnswerComment $action)
+    {
+        $validator = Validator::make($formData,
+            [
+                'body' => 'required',
+            ],
+            [
+                'required' => 'فیلد اجباری است.',
+            ]
+        );
+        $parent_id = $this->comment->id;
+        $product_id = $this->comment->product_id;
+        $validator->validate();
+        $this->resetValidation();
+        $action->execute($formData, $parent_id, $product_id, $this->answerId);
+        $this->dispatchBrowserEvent('swal:alert-success');
+        $this->redirect('/admin/comments');
     }
 
     public function render()
