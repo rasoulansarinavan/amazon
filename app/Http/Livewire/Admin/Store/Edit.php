04c7c970364c2ec58046b8acba851d03cec3709d
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-18 11:07:25 +0330

    added edit component for store section

--- app/Http/Livewire/Admin/Store/Edit.php
+++ app/Http/Livewire/Admin/Store/Edit.php
@@ -2,18 +2,43 @@
 
 namespace App\Http\Livewire\Admin\Store;
 
+use App\Actions\Store\EditStore;
 use App\Models\Product;
+use Illuminate\Support\Facades\Validator;
 use Livewire\Component;
 
 class Edit extends Component
 {
-    public $product_id;
+    public $product_id, $sold_number, $frozen_number, $marketable_number;
 
     public function mount($id)
     {
         $this->product_id = $id;
-        $this->product = Product::query()->where('id', $id)->firstOrFail();
+        $product = $this->product = Product::query()->where('id', $id)->firstOrFail();
+        $this->sold_number = $product->sold_number;
+        $this->frozen_number = $product->frozen_number;
+        $this->marketable_number = $product->marketable_number;
     }
+
+    public function edit($formData, Product $product, EditStore $action)
+    {
+        $validator = Validator::make($formData, [
+            'sold_number' => 'required|integer',
+            'frozen_number' => 'required|integer',
+            'marketable_number' => 'required|integer',
+        ],
+            [
+                '*.required' => 'فیلد ضروری',
+                '*.integer' => 'فیلد بایذ عدد باشد',
+            ]
+        );
+        $validator->validate();
+        $this->resetValidation();
+        $action->execute($formData, $this->product_id);
+        $this->dispatchBrowserEvent('swal:alert-success');
+        $this->redirect('/admin/store');
+    }
+
     public function render()
     {
         return view('livewire.admin.store.edit')->layout('layouts.app-admin');
