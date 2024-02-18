89698c0acc63d7ad7703986618cad088b96164a8
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-18 20:01:33 +0330

    refactor the CommentRepository

--- app/Repositories/CommentRepository.php
+++ app/Repositories/CommentRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 
 use App\Models\Comment;
+use Illuminate\Support\Facades\Auth;
 
 class CommentRepository
 {
@@ -20,5 +21,37 @@ class CommentRepository
     public function DeleteById(int $id)
     {
         Comment::query()->where('id', $id)->delete();
+    }
+
+    public function saveAnswer($formData, $parent_id, $product_id, $answerId): Comment
+    {
+        $comment = Comment::query()->updateOrCreate(
+            [
+                'id' => $answerId
+            ],
+            [
+                'body' => $formData['body'],
+                'parent_id' => $parent_id,
+                'product_id' => $product_id,
+                'author_id' => 1
+            ]
+        );
+        return $comment;
+    }
+
+    public function editComment($formData, $commentId, $parent_id, $product_id): comment
+    {
+        $comment = Comment::query()->updateOrCreate(
+            [
+                'id' => $commentId
+            ],
+            [
+                'body' => $formData['body'],
+                'parent_id' => $parent_id,
+                'product_id' => $product_id,
+                'author_id' => 1
+            ]
+        );
+        return $comment;
     }
 }
