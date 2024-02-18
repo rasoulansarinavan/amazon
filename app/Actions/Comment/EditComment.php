97e0d347d2f7b218d42ffbe9d18fa42886b6af28
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-18 20:02:22 +0330

    added comment edit action

---
+++ app/Actions/Comment/EditComment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Actions\Comment;
+
+use App\Repositories\CommentRepository;
+
+class EditComment
+{
+    public function __construct(
+        private readonly CommentRepository $commentRepository,
+    )
+    {
+    }
+
+    public function execute($formData, $commentId, $parent_id, $product_id): void
+    {
+        $this->commentRepository->editComment($formData, $commentId, $parent_id, $product_id);
+    }
+}
