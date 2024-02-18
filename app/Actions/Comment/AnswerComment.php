7480b0030a90cf0ae0b9db3560bd271dc46477ca
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-18 20:00:54 +0330

    added answer comment action

---
+++ app/Actions/Comment/AnswerComment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Actions\Comment;
+
+use App\Repositories\CommentRepository;
+
+class AnswerComment
+{
+    public function __construct(
+        private readonly CommentRepository $commentRepository,
+    )
+    {
+    }
+
+    public function execute($formData, $parent_id, $product_id, $answerId): void
+    {
+        $this->commentRepository->saveAnswer($formData, $parent_id, $product_id, $answerId);
+    }
+}
