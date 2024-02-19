52f58a302349e7a149ce8dde00db32fd530529f6
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-19 17:47:14 +0330

    create Amazing discount component

---
+++ app/Http/Livewire/Admin/Discount/Amazing.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Discount;
+
+use Livewire\Component;
+
+class Amazing extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.discount.amazing')->layout('layouts.app-admin');
+    }
+}
