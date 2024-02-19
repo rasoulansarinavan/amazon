0dced5f71943bd40a4ed2a7a7d28716afdbb67f5
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-19 18:44:44 +0330

    create common table

---
+++ database/migrations/2024_02_19_151157_create_commons_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('commons', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('percentage');
+            $table->unsignedBigInteger('discount_ceiling')->nullable();
+            $table->unsignedBigInteger('minimal_order_amount')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->timestamp('start_date')->useCurrent();
+            $table->timestamp('end_date')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('commons');
+    }
+};
