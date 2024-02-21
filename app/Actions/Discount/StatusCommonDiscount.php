5b2edc073c5bd6ce85baa93dc3d911d2328c69a1
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-21 11:43:50 +0330

    create StatusCommonDiscount action

---
+++ app/Actions/Discount/StatusCommonDiscount.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Actions\Discount;
+
+use App\Repositories\DiscountRepository;
+
+class StatusCommonDiscount
+{
+    public function __construct(
+        private readonly DiscountRepository $discountRepository
+    )
+    {
+    }
+
+    public function execute($value)
+    {
+        $common = $this->discountRepository->findById($value);
+        if ($common->status == 0) {
+            $this->discountRepository->updateById($value, ['status' => 1]);
+        } elseif ($common->status == 1) {
+            $this->discountRepository->updateById($value, ['status' => 0]);
+        }
+
+    }
+}
