f0266746eca8f6e4955a59fa5eff89855927f335
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-21 11:41:42 +0330

    added saveCoupon in component

--- app/Http/Livewire/Admin/Discount/Coupon.php
+++ app/Http/Livewire/Admin/Discount/Coupon.php
@@ -2,12 +2,53 @@
 
 namespace App\Http\Livewire\Admin\Discount;
 
+use App\Actions\Discount\CreateCouponDiscount;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 use Livewire\Component;
+use \App\Models\Coupon as Coupons;
 
 class Coupon extends Component
 {
+    public $couponId;
+    protected $listeners = ['delete'];
+
+    public function changeType($value)
+    {
+        if ($value == 1) {
+            $this->users = User::query()->get();
+        }
+    }
+
+    public function saveCoupon($formData, Coupons $coupons, CreateCouponDiscount $action)
+    {
+        $validator = Validator::make($formData, [
+            'title' => 'required|string',
+            'code' => 'required',
+            'amount' => 'required|string',
+            'amount_type' => 'required|integer',
+            'discount_ceiling' => 'integer',
+            'type' => 'required|integer',
+            'start_date' => 'required|integer',
+            'end_date' => 'required|integer',
+            'user_id' => 'nullable|integer',
+        ],
+            [
+                '*.required' => 'فیلد ضروری',
+                '*.integer' => 'فیلد بایذ عدد باشد',
+                '*.string' => 'فیلد بایذ رشته باشد',
+
+            ]
+        );
+        $validator->validate();
+        $this->resetValidation();
+        $action->execute($formData, $this->couponId);
+        $this->dispatchBrowserEvent('swal:alert-success');
+    }
+
     public function render()
     {
-        return view('livewire.admin.discount.copan')->layout('layouts.app-admin');
+        return view('livewire.admin.discount.coupon')->layout('layouts.app-admin');
     }
 }
