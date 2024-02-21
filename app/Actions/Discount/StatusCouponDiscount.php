50c1545de18023332adbe1afabaf7bcf7d70b283
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-21 12:37:25 +0330

    added StatusCouponDiscount

--- app/Actions/Discount/StatusCouponDiscount.php
+++ app/Actions/Discount/StatusCouponDiscount.php
@@ -11,4 +11,15 @@ class StatusCouponDiscount
     )
     {
     }
+
+    public function execute($value)
+    {
+        $coupon = $this->discountRepository->findCouponById($value);
+
+        if ($coupon->status == 0) {
+            $this->discountRepository->updateCouponById($value, ['status' => 1]);
+        } elseif ($coupon->status == 1) {
+            $this->discountRepository->updateCouponById($value, ['status' => 0]);
+        }
+    }
 }
