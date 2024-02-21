35345e04c82a4984d4fdb368c8d62ba1eb5ce833
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-21 12:37:04 +0330

    added EditCouponDiscount

---
+++ app/Actions/Discount/EditCouponDiscount.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Actions\Discount;
+
+use App\Repositories\DiscountRepository;
+
+class EditCouponDiscount
+{
+    public function __construct(
+        private readonly DiscountRepository $discountRepository
+    )
+    {
+    }
+
+    public function execute($value)
+    {
+        $coupon = $this->discountRepository->findCouponById($value);
+        $this->title = $coupon->title;
+        $this->code = $coupon->code;
+        $this->amount = $coupon->amount;
+        $this->amount_type = $coupon->amount_type;
+        $this->discount_ceiling = $coupon->discount_ceiling;
+        $this->type = $coupon->type;
+        $this->start_date = $coupon->start_date;
+        $this->end_date = $coupon->end_date;
+        $this->user_id = $coupon->user_id;
+        $this->couponId = $coupon->id;
+    }
+}
