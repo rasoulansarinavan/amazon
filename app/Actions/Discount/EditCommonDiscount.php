0fc999d7cb9e1c0adf8c19b5d815aea2478caa21
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-21 11:45:31 +0330

    added EditCommonDiscount in action

---
+++ app/Actions/Discount/EditCommonDiscount.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Actions\Discount;
+
+use App\Repositories\DiscountRepository;
+
+class EditCommonDiscount
+{
+    public function __construct(
+        private readonly DiscountRepository $discountRepository
+    )
+    {
+    }
+
+    public function execute($value)
+    {
+        $common = $this->discountRepository->findById($value);
+        $this->title = $common->title;
+        $this->percentage = $common->percentage;
+        $this->discount_ceiling = $common->discount_ceiling;
+        $this->minimal_order_amount = $common->minimal_order_amount;
+        $this->start_date = $common->start_date;
+        $this->end_date = $common->end_date;
+        $this->commonId = $common->id;
+    }
+}
