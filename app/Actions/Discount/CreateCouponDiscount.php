82dc207d7959d6898da4312aeeadb7a711ce0fd7
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-02-21 12:35:22 +0330

    added CreateCouponDiscount

---
+++ app/Actions/Discount/CreateCouponDiscount.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Actions\Discount;
+
+use App\Repositories\DiscountRepository;
+
+class CreateCouponDiscount
+{
+    public function __construct(
+        private readonly DiscountRepository $discountRepository
+    )
+    {
+    }
+
+    public function execute($formData, $couponId): void
+    {
+        $this->discountRepository->saveCoupon($formData, $couponId);
+    }
+}
