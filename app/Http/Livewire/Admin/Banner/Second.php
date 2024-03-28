653ff59e3625167cafd6842bc652f806dbc7e9e4
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2024-03-28 19:40:37 +0330

    create view and component for Second section banner in index view

---
+++ app/Http/Livewire/Admin/Banner/Second.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Banner;
+
+use Livewire\Component;
+
+class Second extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.banner.second');
+    }
+}
